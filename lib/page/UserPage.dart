f96205daef93be60ddf2dd329090330a4db18b62
1355081829@qq.com <1355081829aA>
2018-11-01 22:43:24 +0800

    修改个人中心页面展示

--- lib/page/UserPage.dart
+++ lib/page/UserPage.dart
@@ -43,44 +43,105 @@ class _UserPageState extends State<UserPage> {
         margin: const EdgeInsets.all(6.0),
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            new GestureDetector(
-              child: new Container(
-                child: new Row(
-                  children: <Widget>[
-                    new CircleAvatar(
-                      backgroundImage: new NetworkImage(
-                        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541084531572&di=aa88493fe1d2ccfaa64624e83a9982b4&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2Fh_RN2JNiE7CSsFOnmwwgkA%3D%3D%2F2847963814375925996.jpg',
-                      ),
+            new ListTile(
+              title: new Row(
+                children: <Widget>[
+                  new CircleAvatar(
+                    backgroundImage: new NetworkImage(
+                      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541084531572&di=aa88493fe1d2ccfaa64624e83a9982b4&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2Fh_RN2JNiE7CSsFOnmwwgkA%3D%3D%2F2847963814375925996.jpg',
                     ),
-                    new Expanded(
-                      child: new Container(
-                        margin: const EdgeInsets.only(left: 8.0),
-                        child: new Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            new Text(
-                              '寒云',
-                              textScaleFactor: 1.2,
-                            ),
-                            new Text(
-                              '一种相思，两处闲愁',
-                              textScaleFactor: .9,
-                            ),
-                          ],
-                        ),
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                  ),
+                  new Align(
+                    alignment: Alignment.centerLeft,
+                    child: new Text(
+                      '寒云',
+                      textScaleFactor: 1.2,
                     ),
-                  ],
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                ),
+                  )
+                ],
+              ),
+              subtitle: new Text(
+                ' 故天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能',
+                style: new TextStyle(),
+                overflow: TextOverflow.ellipsis,
               ),
               onTap: () {
                 Navigator.of(context).push(MaterialPageRoute(builder: (_) {
                   return new UserInfo();
                 }));
               },
+              trailing: new Icon(Icons.keyboard_arrow_right),
+            ),
+            new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new Expanded(
+                  child: new ListTile(
+                    title: new Text(
+                      '11',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: new Text(
+                      '粉丝',
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {
+                      print(111);
+                    },
+                  ),
+                ),
+                new Expanded(
+                  child: new ListTile(
+                    title: new Text(
+                      '111',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: new Text(
+                      '关注',
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {
+                      print(111);
+                    },
+                  ),
+                ),
+                new Expanded(
+                  child: new ListTile(
+                    title: new Text(
+                      '11',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: new Text(
+                      '社团',
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {
+                      print(111);
+                    },
+                  ),
+                ),
+                new Expanded(
+                  child: new ListTile(
+                    title: new Text(
+                      '11',
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: new Text(
+                      '校园',
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {
+                      print(111);
+                    },
+                  ),
+                ),
+              ],
             ),
             new Expanded(
               child: new ListView(
