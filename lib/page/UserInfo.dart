cdde5ee4140944e6fade060237a5d961562e35db
hanyun <1355081829@qq.com>
2018-11-02 10:38:38 +0800

    个人信息

--- lib/page/UserInfo.dart
+++ lib/page/UserInfo.dart
@@ -8,34 +8,187 @@ class UserInfo extends StatefulWidget {
 }
 
 class _UserInfoState extends State<UserInfo> {
+  String _backgroundImg =
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541084531572&di=aa88493fe1d2ccfaa64624e83a9982b4&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2Fh_RN2JNiE7CSsFOnmwwgkA%3D%3D%2F2847963814375925996.jpg';
+  String _avatarImg =
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541084531572&di=aa88493fe1d2ccfaa64624e83a9982b4&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2Fh_RN2JNiE7CSsFOnmwwgkA%3D%3D%2F2847963814375925996.jpg';
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(),
+      appBar: new AppBar(
+        title: new Center(
+          child: new Text('个人信息'),
+        ),
+      ),
       body: new Container(
-        child: new Stack(
+        child: new Column(
           children: <Widget>[
-            new Container(
-              decoration: new BoxDecoration(
-                color: Colors.white,
-                image: new DecorationImage(
-                    image: new NetworkImage(
-                        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541084531572&di=aa88493fe1d2ccfaa64624e83a9982b4&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2Fh_RN2JNiE7CSsFOnmwwgkA%3D%3D%2F2847963814375925996.jpg'),
-                    fit: BoxFit.cover),
-              ),
-              height: 200.0,
-              child: new Container(
-                alignment: Alignment.bottomCenter,
-                child: new CircleAvatar(
-                  backgroundImage: new NetworkImage(
-                      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541084531572&di=aa88493fe1d2ccfaa64624e83a9982b4&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2Fh_RN2JNiE7CSsFOnmwwgkA%3D%3D%2F2847963814375925996.jpg'),
-                  radius: 40.0,
+            new UserAvatarWithBackgroundImage(
+              backgroundImg: _backgroundImg,
+              avatarImg: _avatarImg,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: new GestureDetector(
+                child: new Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: new Text(
+                        "故天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能",
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    new Icon(Icons.edit)
+                  ],
                 ),
+                onTap: () {
+                  print(111);
+                },
               ),
+              padding: const EdgeInsets.all(8.0),
+            ),
+            new ListTile(
+              title: new Row(
+                children: <Widget>[
+                  new Text('昵称'),
+                  new Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: new Text('寒云'),
+                    ),
+                  )
+                ],
+              ),
+              trailing: new Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            new ListTile(
+              title: new Row(
+                children: <Widget>[
+                  new Text('生日'),
+                  new Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: new Text('1989-03-18'),
+                    ),
+                  )
+                ],
+              ),
+              trailing: new Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            new ListTile(
+              title: new Row(
+                children: <Widget>[
+                  new Text('学校'),
+                  new Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: new Text('河南工业大学'),
+                    ),
+                  )
+                ],
+              ),
+              trailing: new Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            new ListTile(
+              title: new Row(
+                children: <Widget>[
+                  new Text('入学年份'),
+                  new Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: new Text('2010'),
+                    ),
+                  )
+                ],
+              ),
+              trailing: new Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            new ListTile(
+              title: new Row(
+                children: <Widget>[
+                  new Text('故乡'),
+                  new Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: new Text('河南-许昌'),
+                    ),
+                  )
+                ],
+              ),
+              trailing: new Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            new ListTile(
+              title: new Row(
+                children: <Widget>[
+                  new Text('专业'),
+                  new Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: new Text('经济学'),
+                    ),
+                  )
+                ],
+              ),
+              trailing: new Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class UserAvatarWithBackgroundImage extends StatefulWidget {
+  final String backgroundImg;
+  final double backgroundHeight;
+  final String avatarImg;
+  final double avatarHeight;
+
+  UserAvatarWithBackgroundImage({
+    this.backgroundImg,
+    this.backgroundHeight = 200.0,
+    this.avatarImg,
+    this.avatarHeight = 40.0,
+  });
+
+  @override
+  State createState() {
+    return new _UserAvatarWithBackgroundImageState();
+  }
+}
+
+class _UserAvatarWithBackgroundImageState
+    extends State<UserAvatarWithBackgroundImage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Stack(
+        children: <Widget>[
+          new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                  image: new NetworkImage(widget.backgroundImg),
+                  fit: BoxFit.cover),
+            ),
+            height: widget.backgroundHeight,
+          ),
+          new Align(
+            alignment: Alignment.bottomCenter,
+            child: new CircleAvatar(
+              backgroundImage: new NetworkImage(widget.avatarImg),
+              radius: widget.avatarHeight,
+            ),
+          )
+        ],
+      ),
+      height: widget.backgroundHeight + widget.avatarHeight,
     );
   }
 }
