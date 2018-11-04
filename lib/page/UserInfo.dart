137c6198412a40cd3c62e689e38dbd16b7c1057d
1355081829@qq.com <1355081829aA>
2018-11-04 15:04:15 +0800

    编辑个人信息页面

--- lib/page/UserInfo.dart
+++ lib/page/UserInfo.dart
@@ -23,194 +23,193 @@ class _UserInfoState extends State<UserInfo> {
         ),
       ),
       body: new Container(
-        child: new Column(
-          children: <Widget>[
-            new UserAvatarWithBackgroundImage(
-              backgroundImg: _backgroundImg,
-              avatarImg: _avatarImg,
+        child: new ListView(children: <Widget>[
+          new UserAvatarWithBackgroundImage(
+            backgroundImg: _backgroundImg,
+            avatarImg: _avatarImg,
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: new GestureDetector(
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new Text(
+                      "故天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能",
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  new Icon(Icons.edit)
+                ],
+              ),
+              onTap: () {
+                print(111);
+              },
             ),
-            Container(
-              alignment: Alignment.center,
-              child: new GestureDetector(
-                child: new Row(
-                  children: <Widget>[
-                    new Expanded(
-                      child: new Text(
-                        "故天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能",
-                        overflow: TextOverflow.ellipsis,
+            padding: const EdgeInsets.all(8.0),
+          ),
+          new ListTile(
+            title: new Row(
+              children: <Widget>[
+                new Text('昵称'),
+                new Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: new Text('寒云'),
+                  ),
+                )
+              ],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Row(
+              children: <Widget>[
+                new Text('生日'),
+                new Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: new Text('1989-03-18'),
+                  ),
+                )
+              ],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              DatePicker.showDatePicker(
+                context,
+                showTitleActions: true,
+                locale: 'zh',
+                minYear: 1970,
+                maxYear: new DateTime.now().year,
+                initialYear: new DateTime.now().year,
+                initialMonth: 6,
+                initialDate: 21,
+                dateFormat: 'yyyy-mm-dd',
+                onChanged: (year, month, date) {},
+                onConfirm: (year, month, date) {
+                  print(year);
+                },
+              );
+            },
+          ),
+          new ListTile(
+            title: new Row(
+              children: <Widget>[
+                new Text('学校'),
+                new Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: new Text('河南工业大学'),
+                  ),
+                )
+              ],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Row(
+              children: <Widget>[
+                new Text('入学年份'),
+                new Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: new Text('2010'),
+                  ),
+                )
+              ],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              DatePicker.showDatePicker(
+                context,
+                showTitleActions: true,
+                locale: 'zh',
+                minYear: 1970,
+                maxYear: new DateTime.now().year,
+                initialYear: new DateTime.now().year,
+                initialMonth: 6,
+                initialDate: 21,
+                dateFormat: 'yyyy',
+                onChanged: (year, month, date) {},
+                onConfirm: (year, month, date) {
+                  print(year);
+                },
+              );
+            },
+          ),
+          new ListTile(
+            title: new Row(
+              children: <Widget>[
+                new Text('故乡'),
+                new Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: new Text('河南-许昌'),
+                  ),
+                )
+              ],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              showModalBottomSheet<Null>(
+                context: context,
+                builder: (BuildContext context) {
+                  return new Container(
+                    child: new Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: new ListView.builder(
+                        itemBuilder: (context, index) {
+                          return new Text('河南省');
+                        },
+                        itemCount: 200,
                       ),
                     ),
-                    new Icon(Icons.edit)
+                  );
+                },
+              );
+            },
+          ),
+          new ListTile(
+            title: new Row(
+              children: <Widget>[
+                new Text('专业'),
+                new Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: new Text('经济学'),
+                  ),
+                )
+              ],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) => new AlertDialog(
+                  title: new Text(""),
+                  content: new Text('aaaaaaa'),
+                  actions: <Widget>[
+                    new FlatButton(
+                      child: new Text("取消"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    new FlatButton(
+                      child: new Text("确定"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
                   ],
                 ),
-                onTap: () {
-                  print(111);
-                },
-              ),
-              padding: const EdgeInsets.all(8.0),
-            ),
-            new ListTile(
-              title: new Row(
-                children: <Widget>[
-                  new Text('昵称'),
-                  new Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: new Text('寒云'),
-                    ),
-                  )
-                ],
-              ),
-              trailing: new Icon(Icons.keyboard_arrow_right),
-              onTap: () {},
-            ),
-            new ListTile(
-              title: new Row(
-                children: <Widget>[
-                  new Text('生日'),
-                  new Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: new Text('1989-03-18'),
-                    ),
-                  )
-                ],
-              ),
-              trailing: new Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                DatePicker.showDatePicker(
-                  context,
-                  showTitleActions: true,
-                  locale: 'zh',
-                  minYear: 1970,
-                  maxYear: new DateTime.now().year,
-                  initialYear: new DateTime.now().year,
-                  initialMonth: 6,
-                  initialDate: 21,
-                  dateFormat: 'yyyy-mm-dd',
-                  onChanged: (year, month, date) {},
-                  onConfirm: (year, month, date) {
-                    print(year);
-                  },
-                );
-              },
-            ),
-            new ListTile(
-              title: new Row(
-                children: <Widget>[
-                  new Text('学校'),
-                  new Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: new Text('河南工业大学'),
-                    ),
-                  )
-                ],
-              ),
-              trailing: new Icon(Icons.keyboard_arrow_right),
-              onTap: () {},
-            ),
-            new ListTile(
-              title: new Row(
-                children: <Widget>[
-                  new Text('入学年份'),
-                  new Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: new Text('2010'),
-                    ),
-                  )
-                ],
-              ),
-              trailing: new Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                DatePicker.showDatePicker(
-                  context,
-                  showTitleActions: true,
-                  locale: 'zh',
-                  minYear: 1970,
-                  maxYear: new DateTime.now().year,
-                  initialYear: new DateTime.now().year,
-                  initialMonth: 6,
-                  initialDate: 21,
-                  dateFormat: 'yyyy',
-                  onChanged: (year, month, date) {},
-                  onConfirm: (year, month, date) {
-                    print(year);
-                  },
-                );
-              },
-            ),
-            new ListTile(
-              title: new Row(
-                children: <Widget>[
-                  new Text('故乡'),
-                  new Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: new Text('河南-许昌'),
-                    ),
-                  )
-                ],
-              ),
-              trailing: new Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                showModalBottomSheet<Null>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return new Container(
-                      child: new Padding(
-                        padding: const EdgeInsets.all(32.0),
-                        child: new ListView.builder(
-                          itemBuilder: (context, index) {
-                            return new Text('河南省');
-                          },
-                          itemCount: 200,
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-            new ListTile(
-              title: new Row(
-                children: <Widget>[
-                  new Text('专业'),
-                  new Expanded(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: new Text('经济学'),
-                    ),
-                  )
-                ],
-              ),
-              trailing: new Icon(Icons.keyboard_arrow_right),
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => new AlertDialog(
-                        title: new Text(""),
-                        content: new Text('aaaaaaa'),
-                        actions: <Widget>[
-                          new FlatButton(
-                            child: new Text("取消"),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          new FlatButton(
-                            child: new Text("确定"),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          )
-                        ],
-                      ),
-                );
-              },
-            ),
-          ],
+              );
+            },
+          ),
+        ],
         ),
       ),
     );
