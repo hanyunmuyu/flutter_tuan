88c96343634056df8633a2cae1399263f37a1c23
1355081829@qq.com <1355081829aA>
2018-11-07 22:19:39 +0800

    增加高校页面

--- lib/page/SchoolActivePage.dart
+++ lib/page/SchoolActivePage.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 
 class SchoolActivePage extends StatelessWidget {
+  List<Widget> children = <Widget>[
+    new Center(
+      child: new Text('榜单推荐'),
+    ),
+    new ListTile(
+      leading: new Text('相关[校园]'),
+      title: new Text('1'),
+    ),
+    new ListTile(
+      leading: new Text('相关[社团]'),
+      title: new Text('1'),
+    ),
+    new ListTile(
+      leading: new Text('热门[校园]'),
+      title: new Text('1'),
+    ),
+    new ListTile(
+      leading: new Text('热门[国内]'),
+      title: new Text('1'),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return new Text('SchoolActivePage');
+    return new Container(
+      width: double.infinity,
+      child: new Column(
+        children: children,
+      ),
+    );
   }
 }
