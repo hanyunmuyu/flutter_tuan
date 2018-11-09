5e90033bc8459964450566ceb8404db7e264c4e7
hanyun <1355081829@qq.com>
2018-11-09 14:34:31 +0800

    个人中心，我的状态判断

--- lib/page/UserPage.dart
+++ lib/page/UserPage.dart
@@ -72,9 +72,8 @@ class _UserPageState extends State<UserPage> {
                   title: new Row(
                     children: <Widget>[
                       new CircleAvatar(
-                        backgroundImage: new NetworkImage(
-                          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541084531572&di=aa88493fe1d2ccfaa64624e83a9982b4&imgtype=0&src=http%3A%2F%2Fimg2.ph.126.net%2Fh_RN2JNiE7CSsFOnmwwgkA%3D%3D%2F2847963814375925996.jpg',
-                        ),
+                        backgroundImage:
+                            new NetworkImage(store.state.user.data['avatar']),
                       ),
                       Padding(
                         padding: const EdgeInsets.only(left: 8.0),
@@ -89,7 +88,7 @@ class _UserPageState extends State<UserPage> {
                     ],
                   ),
                   subtitle: new Text(
-                    ' 故天将降大任于斯人也，必先苦其心志，劳其筋骨，饿其体肤，空乏其身，行拂乱其所为，所以动心忍性，曾益其所不能',
+                    store.state.user.data['motto'],
                     style: new TextStyle(),
                     overflow: TextOverflow.ellipsis,
                   ),
