04bc1320bfeddf2cbdecdba7c73bfd9977414986
hanyun <1355081829@qq.com>
2018-11-15 15:28:30 +0800

    地区三级联动

--- lib/widget/city_picker.dart
+++ lib/widget/city_picker.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+typedef void ChangeData(int index);
+
 class CityPicker {
   static void showCityPicker(BuildContext context) {
     Navigator.push(
@@ -71,47 +73,74 @@ class _CityPickerWidget extends StatefulWidget {
 class _CityPickerState extends State<_CityPickerWidget> {
   FixedExtentScrollController fixedExtentScrollController =
       new FixedExtentScrollController();
+  List<Map<String, dynamic>> data = [
+    {"id": 1, "name": "1111"},
+    {"id": 1, "name": "1111"},
+  ];
+  List<Map<String, dynamic>> data1 = [
+    {"id": 1, "name": "1111"},
+  ];
 
   Widget _bottomView() {
+    Widget province = new _Picker(
+      data: data,
+      changeData: (index) {
+        setState(() {
+          data1.add(data[index]);
+        });
+      },
+    );
+    Widget city = new _Picker(
+      data: data1,
+      changeData: (index) {},
+    );
     return new Container(
       alignment: Alignment.bottomCenter,
       color: Colors.white,
       width: double.infinity,
       height: 210.0,
       child: new Column(
+        mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          new Row(
-            children: <Widget>[
-              FlatButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: new Text(
-                  '取消',
-                  style: new TextStyle(
-                      color: Theme.of(context).unselectedWidgetColor),
+          new Container(
+            height: 40.0,
+            width: double.infinity,
+            child: new Row(
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: new Text(
+                    '取消',
+                    style: new TextStyle(
+                        color: Theme.of(context).unselectedWidgetColor),
+                  ),
                 ),
-              ),
-              FlatButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: new Text(
-                  '确定',
-                  style: new TextStyle(
-                      color: Theme.of(context).unselectedWidgetColor),
+                FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: new Text(
+                    '确定',
+                    style: new TextStyle(
+                        color: Theme.of(context).unselectedWidgetColor),
+                  ),
                 ),
-              ),
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ),
           ),
-          new Row(
-            children: <Widget>[
-              new _Picker(),
-              new _Picker(),
-              new _Picker(),
-            ],
-          )
+          new Expanded(
+            flex: 1,
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                province,
+                city,
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -141,6 +170,11 @@ class _CityPickerState extends State<_CityPickerWidget> {
 }
 
 class _Picker extends StatefulWidget {
+  final List<Map<String, dynamic>> data;
+  final ChangeData changeData;
+
+  _Picker({this.data, this.changeData});
+
   @override
   State createState() {
     return new _PickerState();
@@ -152,7 +186,29 @@ class _PickerState extends State<_Picker> {
   Widget build(BuildContext context) {
     return new Expanded(
       flex: 1,
-      child: new Text('1'),
+      child: new Container(
+        margin: const EdgeInsets.all(6.0),
+        alignment: Alignment.center,
+        child: new CupertinoPicker(
+          backgroundColor: Colors.white,
+          itemExtent: 30.0,
+          onSelectedItemChanged: (index) {
+            if (widget.changeData != null) {
+              widget.changeData(index);
+            }
+          },
+          children: widget.data.map((v) {
+            return new Container(
+              alignment: Alignment.centerLeft,
+              width: double.infinity,
+              child: new Text(
+                v.toString(),
+                textScaleFactor: 1.2,
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
