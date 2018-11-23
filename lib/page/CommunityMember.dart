6aeefe44265c8186d55be4281a21bef020244883
hanyun <1355081829@qq.com>
2018-11-23 16:04:47 +0800

    图片查看器

--- lib/page/CommunityMember.dart
+++ lib/page/CommunityMember.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tuan/widget/picture_show.dart';
 
 class CommunityMember extends StatelessWidget {
   final int id;
+  String img = 'http://pic1.win4000.com/wallpaper/2018-01-13/5a59bb362eb18.jpg';
+  String img1 =
+      'http://pic1.win4000.com/wallpaper/2018-08-30/5b878eec0d4cf.jpg';
 
   CommunityMember(this.id);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.builder(
-        itemBuilder: (context, index) => ListTile(
-              title: Text('$index'),
-            ),
-        itemCount: 100,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: PictureShow(
+        picList: [
+          img,
+          img1,
+          img,
+          img1,
+          img1,
+          img,
+          img1
+        ],
       ),
     );
   }
